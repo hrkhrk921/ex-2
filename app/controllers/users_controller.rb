66d4806a6f79e8d8b02152bfd317644c328b3095
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-05-17 20:55:37 +0900

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 	before_action :baria_user, only: [:update]
 
   def show
-  	@user = User.find(params[:id])
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施)
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
@@ -16,16 +17,22 @@ class UsersController < ApplicationController
 
   def edit
   	@user = User.find(params[:id])
+    @books = Book.all
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
-  		redirect_to users_path(@user), notice: "successfully updated user!"
+  		redirect_to user_path(@user.id), notice: "successfully updated user!"
   	else
-  		render "show"
+      notice = "error"
+  		render "edit"
   	end
   end
+  
 
   private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to:'home#top'
   get 'home/about', to:'home#about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books,only: [:new, :create, :index, :show, :edit, :destroy, :update]
+  resources :users
 end 
